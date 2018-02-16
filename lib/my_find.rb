1b5988d5e5321dd768ef733a356ea4011c121025
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-16 20:10:02 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,6 +4,8 @@ def my_find(collection)
   i = 0
   some_new_array = [ ]
   while (i < collection.length)
+    if yield(collection[i])
+      return (collection[i])
     some_new_array << yield(collection[i])
     i = i + 1
   end
